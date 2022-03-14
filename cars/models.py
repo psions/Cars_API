4eecceaff0b6ec63f7e447f1162c3f175326a4e9
psions <psionreality@gmail.com>
2022-03-14 13:47:22 -0500

    Fixed Erros from wrong code in views table

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+# Create your views here.
+class Car(models.Model):
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
 # Create your models here.
